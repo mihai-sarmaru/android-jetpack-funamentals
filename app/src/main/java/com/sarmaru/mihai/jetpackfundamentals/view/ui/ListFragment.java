517308b038464acbb559799d4e2133ecf4681ce0
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-10-14 12:05:26 +0300

    Use ButterKnife to bind views

--- app/src/main/java/com/sarmaru/mihai/jetpackfundamentals/view/ui/ListFragment.java
+++ app/src/main/java/com/sarmaru/mihai/jetpackfundamentals/view/ui/ListFragment.java
@@ -8,9 +8,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.sarmaru.mihai.jetpackfundamentals.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ListFragment extends Fragment {
+
+    @BindView(R.id.floatingActionButtonList)
+    FloatingActionButton floatingActionButtonList;
 
     public ListFragment() {
     }
@@ -18,6 +25,8 @@ public class ListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 }
