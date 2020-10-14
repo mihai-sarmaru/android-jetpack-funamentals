b204225f3b32cf81638c1ec0a52e5d4d5dfed644
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-10-14 12:26:44 +0300

    Add UP navigation to MainActivity

--- app/src/main/java/com/sarmaru/mihai/jetpackfundamentals/view/ui/MainActivity.java
+++ app/src/main/java/com/sarmaru/mihai/jetpackfundamentals/view/ui/MainActivity.java
@@ -1,6 +1,10 @@
 package com.sarmaru.mihai.jetpackfundamentals.view.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
@@ -8,9 +12,19 @@ import com.sarmaru.mihai.jetpackfundamentals.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navController = Navigation.findNavController(this, R.id.fragmentMainActivity);
+        NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, (DrawerLayout) null);
     }
 }
