646d0adde30f122c5538c0f90607c95ce1dac902
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-10-14 11:27:00 +0300

    Add blank fragments for List and Detail views

---
+++ app/src/main/java/com/sarmaru/mihai/jetpackfundamentals/view/ui/DetailFragment.java
@@ -0,0 +1,23 @@
+package com.sarmaru.mihai.jetpackfundamentals.view.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sarmaru.mihai.jetpackfundamentals.R;
+
+public class DetailFragment extends Fragment {
+
+    public DetailFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+}
---
+++ app/src/main/java/com/sarmaru/mihai/jetpackfundamentals/view/ui/ListFragment.java
@@ -0,0 +1,23 @@
+package com.sarmaru.mihai.jetpackfundamentals.view.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sarmaru.mihai.jetpackfundamentals.R;
+
+public class ListFragment extends Fragment {
+
+    public ListFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+}
