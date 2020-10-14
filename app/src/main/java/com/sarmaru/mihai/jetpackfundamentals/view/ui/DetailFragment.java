7dbe5406ac715cb216034e519719f5d1639a5db3
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-10-14 12:37:47 +0300

    use navigation on view to navigate to list fragment

--- app/src/main/java/com/sarmaru/mihai/jetpackfundamentals/view/ui/DetailFragment.java
+++ app/src/main/java/com/sarmaru/mihai/jetpackfundamentals/view/ui/DetailFragment.java
@@ -2,15 +2,26 @@ package com.sarmaru.mihai.jetpackfundamentals.view.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.sarmaru.mihai.jetpackfundamentals.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DetailFragment extends Fragment {
+
+    @BindView(R.id.floatingActionButtonDetail)
+    FloatingActionButton floatingActionButtonDetail;
 
     public DetailFragment() {
     }
@@ -18,6 +29,20 @@ public class DetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Uses lambda expression
+        floatingActionButtonDetail.setOnClickListener(v -> onGoToList());
+    }
+
+    private void onGoToList() {
+        NavDirections action = DetailFragmentDirections.actionList();
+        Navigation.findNavController(floatingActionButtonDetail).navigate(action);
     }
 }
