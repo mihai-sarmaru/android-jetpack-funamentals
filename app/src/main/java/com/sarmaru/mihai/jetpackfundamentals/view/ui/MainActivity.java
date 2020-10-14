eb780ede33429dcc28f4202a7c3184f555b0b08a
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-10-14 11:22:22 +0300

    Move MainActivity to view.ui package

--- app/src/main/java/com/sarmaru/mihai/jetpackfundamentals/view/ui/MainActivity.java
+++ app/src/main/java/com/sarmaru/mihai/jetpackfundamentals/view/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.sarmaru.mihai.jetpackfundamentals;
+package com.sarmaru.mihai.jetpackfundamentals.view.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.sarmaru.mihai.jetpackfundamentals.R;
 
 public class MainActivity extends AppCompatActivity {
 
