b7c2a90f2fcae32d4e44626224fdd6cc9cb678e0
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-10-20 17:10:37 +0300

    Add fetchFromDatabase or fetchFromRemote refresh interval

--- app/src/main/java/com/sarmaru/mihai/jetpackfundamentals/viewmodel/DogListViewModel.java
+++ app/src/main/java/com/sarmaru/mihai/jetpackfundamentals/viewmodel/DogListViewModel.java
@@ -12,6 +12,7 @@ import com.sarmaru.mihai.jetpackfundamentals.db.DogBreedDao;
 import com.sarmaru.mihai.jetpackfundamentals.db.DogDatabase;
 import com.sarmaru.mihai.jetpackfundamentals.model.DogBreed;
 import com.sarmaru.mihai.jetpackfundamentals.repository.DogApiService;
+import com.sarmaru.mihai.jetpackfundamentals.util.SharedPreferencesHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,13 +35,22 @@ public class DogListViewModel extends AndroidViewModel {
     private AsyncTask<List<DogBreed>, Void, List<DogBreed>> insertTask;
     private AsyncTask<Void, Void, List<DogBreed>> retrieveDogsTask;
 
+    private SharedPreferencesHelper preferencesHelper = SharedPreferencesHelper.getInstance(getApplication());
+    private long refreshTime = 5 * 60 * 1000 * 1000 * 1000L; // 5 minutes
+
     public DogListViewModel(@NonNull Application application) {
         super(application);
     }
 
     public void refresh() {
-        fetchFromRemote();
-//        fetchFromDatabase();
+        long updateTime = preferencesHelper.getUpdateTime();
+        long currentTime = System.nanoTime();
+
+        if (updateTime != 0 && currentTime - updateTime < refreshTime) {
+            fetchFromDatabase();
+        } else {
+            fetchFromRemote();
+        }
     }
 
     public void fetchFromDatabase() {
@@ -121,6 +131,7 @@ public class DogListViewModel extends AndroidViewModel {
         @Override
         protected void onPostExecute(List<DogBreed> dogBreeds) {
             dogsRetrieved(dogBreeds);
+            preferencesHelper.saveUpdateTime(System.nanoTime());
         }
     }
 
