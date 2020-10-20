53da0af165ee0c511c77f554f71b9687113c49bc
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-10-20 17:40:08 +0300

    Get Dog from Database with AsyncTask

--- app/src/main/java/com/sarmaru/mihai/jetpackfundamentals/viewmodel/DogDetailViewModel.java
+++ app/src/main/java/com/sarmaru/mihai/jetpackfundamentals/viewmodel/DogDetailViewModel.java
@@ -1,14 +1,18 @@
 package com.sarmaru.mihai.jetpackfundamentals.viewmodel;
 
 import android.app.Application;
+import android.os.AsyncTask;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 
+import com.sarmaru.mihai.jetpackfundamentals.db.DogDatabase;
 import com.sarmaru.mihai.jetpackfundamentals.model.DogBreed;
 
 public class DogDetailViewModel extends AndroidViewModel {
+
+    private RetrieveDogTask task;
 
     public MutableLiveData<DogBreed> dogLiveData = new MutableLiveData<DogBreed>();
 
@@ -16,9 +20,33 @@ public class DogDetailViewModel extends AndroidViewModel {
         super(application);
     }
 
-    public void getLiveData() {
-        // Mock data
-        DogBreed dog = new DogBreed("1", "corgi", "15 years", "", "companion", "calm", "");
-        dogLiveData.setValue(dog);
+    public void fetch(int uuid) {
+        task = new RetrieveDogTask();
+        task.execute(uuid);
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+
+        // Clear task to prevent memory leak
+        if (task != null) {
+            task.cancel(true);
+            task = null;
+        }
+    }
+
+    private class RetrieveDogTask extends AsyncTask<Integer, Void, DogBreed> {
+
+        @Override
+        protected DogBreed doInBackground(Integer... integers) {
+            int uuid = integers[0];
+            return DogDatabase.getInstance(getApplication()).dogBreedDao().getDog(uuid);
+        }
+
+        @Override
+        protected void onPostExecute(DogBreed dogBreed) {
+            dogLiveData.setValue(dogBreed);
+        }
     }
 }
